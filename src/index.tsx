95d65e6b9892f7c4206ff751061cbc4d1e7711d4
Adil Peter <adilnesmith@ymail.com>
2022-08-04 14:21:18 +0500

    refactor: add variables file for stylessheet so styles can be modified thoroughly by changing the values

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Modal from "react-modal"
-import "./index.css";
+import "styles/index.scss";
 import { ShopApp } from "./components/shop-app/shop-app";
 import * as serviceWorker from "./serviceWorker";
 
