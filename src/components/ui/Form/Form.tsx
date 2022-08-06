723a747479d18dc220c6fb58f344a1abc15ef743
Adil Peter <adilnesmith@ymail.com>
2022-08-06 11:52:31 +0500

    chore: add the senity checks so that code not breaks on any point

--- src/components/ui/Form/Form.tsx
+++ src/components/ui/Form/Form.tsx
@@ -14,55 +14,44 @@ export const Form: React.FC<FormProps> = (props) => {
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
-
-    if (!titleRef.current?.value) {
+    if (!titleRef?.current?.value) {
       alert("Your product needs a title");
-
       return;
     }
-
-    if (!descriptionRef.current?.value || !priceRef.current?.value) {
+    if (!descriptionRef?.current?.value || !priceRef?.current?.value) {
       alert("Your product needs some content");
-
       return;
     }
-
     props["on-submit"]({
-      title: titleRef.current && titleRef.current.value,
-      description: descriptionRef.current && descriptionRef.current.value,
-      price: priceRef.current && priceRef.current.value,
+      title: titleRef?.current?.value,
+      description: descriptionRef?.current.value,
+      price: priceRef?.current?.value,
     });
-
-    formRef.current?.reset();
+    formRef?.current?.reset();
   };
 
   return (
     <form className={styles.form} onSubmit={(event) => handleSubmit(event)} ref={formRef}>
-      <Label className={styles.label} children={'Product title: *'}></Label>
-
+      <Label className={styles.form__label} children={'Product title: *'}></Label>
       <Input
         inputRef={titleRef}
         placeholder="Title..."
         defaultValue=""
-        className={styles.input}
+        className={styles.form__input}
       />
-
-      <Label className={styles.label} children={'Product details: *'}></Label>
-
+      <Label className={styles.form__label} children={'Product details: *'}></Label>
       <Input
         inputRef={priceRef}
         placeholder="Price..."
         defaultValue=""
-        className={styles.input}
+        className={styles.form__input}
       />
-
       <TextArea
         inputRef={descriptionRef}
         placeholder="Start typing product description here..."
         defaultValue=""
-        className={styles.textarea}
+        className={styles.form__textarea}
       />
-
       <Button children={'Add a product'}></Button>
     </form>
   );
